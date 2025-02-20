e53e5557109323f758dfcd943a47aca2aa1d9f35
Anisur Rahaman <153162492+maruf320101@users.noreply.github.com>
2025-02-20 21:38:25 +0600

    Add files via upload

---
+++ solve.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class solve {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        
+        int n = scr.nextInt(); // Read the number of problems
+        int count = 0; // Count the number of problems they will solve
+
+        for (int i = 0; i < n; i++) {
+            int petya = scr.nextInt();
+            int vasya = scr.nextInt();
+            int tonya = scr.nextInt();
+
+            // If at least two of them are sure, they will solve the problem
+            if (petya + vasya + tonya >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count); // Print the final count
+        scr.close();
+    }
+}
